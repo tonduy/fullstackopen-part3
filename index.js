6e72ba7ddf92efef36dcddefcf630f50e953795d
tonduy <80115135+tonduy@users.noreply.github.com>
2023-06-29 15:31:31 +0200

    Finished to exercise part3 Phonebook 3.8

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan');
 app.use(express.json())
+
+morgan.token("postData", req => {
+    return req.method === "POST" ? JSON.stringify(req.body) : '';
+});
+
+app.use(
+    morgan(':method :url :status :res[content-length] - :response-time ms :postData')
+);
 
 let phonebook = [
     {
